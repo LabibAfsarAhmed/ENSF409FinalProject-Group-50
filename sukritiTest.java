2b4b822ddd035aec212e57d62925b5d186f67ee0
Sukriti sharma <ss1106connellycourt@gmail.com>
2022-04-01 12:45:15 -0600

    commit 1

--- sukritiTest.java
+++ sukritiTest.java
@@ -1,3 +1,51 @@
+package edu.ucalgary.ensf409;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.io.*;
+import java.util.*;
+
 public class sukritiTest {
     // Hello 
+
+
+    // Test the constructor for ConfigureHamper
+    @Test
+    public void testConfigureHamper() {
+        ConfigureHamper hamper = new ConfigureHamper(orderForm);
+        assertNotNull("ConfigureHamper constructor did not create an object.",
+                hamper);
+    }
+
+
+
+    @Test
+    public void testCalculateHamper(){
+        int hamper1Calc = calculateHamper(hamper1);
+        int expectedHamper1Calc = 1067;
+        assertEquals("Method CalculateHamper did not return the expected result ", hamper1Calc, expectedHamper1Calc);
+    }
+
+    @Test
+    public void testGetNewRequest(){
+        ConfigureHamper hamper = new ConfigureHamper(orderForm);
+        String expectedRequest = "1 Adult Female, 2 Child under 8";
+        String foundRequest = getNewRequest(orderform);       //FIX THIS
+        assertEquals("Method getNewRequest did not return the expected result ", expectedRequest, foundRequest);
+        
+
+    }
+
+    @Test 
+    public void testSetNewRequest(){
+        ConfigureHamper hamper = new ConfigureHamper(orderform);
+        hamper.setNewRequest("1 Adult Male, 1 Child over 8");    //GENERALIZE TO MAKE A ITERATOR FOR EACH HAMPER TO READ IN FROM FILE
+        String foundRequest = hamper.getNewRequest();
+        String expectedRequest = "1 Adult Male, 1 Child over 8";
+        assertEquals("Method setNewRequest did not return the expected result: ", expectedRequest, foundRequest);
+    }
+
 }
+
+
+
