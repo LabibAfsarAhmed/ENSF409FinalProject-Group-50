93cf7b3865968718945be48d2bdc228076b1758c
CarolineBasta <carolinebasta95@gmail.com>
2022-04-01 20:55:00 -0600

    commit 1

--- carolineTest.java
+++ carolineTest.java
@@ -1,3 +1,24 @@
+import org.junit.Test;
+
+import jdk.jfr.Timestamp;
+
+import org.junit.Assert;
+import static org.junit.Assert.*;
+
+
 public class carolineTest {
-    // Hello 
+    //testing for shortage in inventory 
+    @Test
+    public void testShortageException(){
+        Inventroy inventory = new Inventory ("inventory.sql");
+        inventory.getStock(Inventory);
+
+        try{
+            inventory.ShortageException();
+            fail("Error: shortage in inventory");
+        }
+        catch (Excetion e){
+            assertTrue(True);
+        }
+    }
 }
