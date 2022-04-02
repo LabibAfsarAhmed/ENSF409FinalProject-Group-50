3f22720ceebc7c7e84387b6ef30801a5ccdfe6cc
Labib Afsar Ahmed <labibafsarahmed02@gmail.com>
2022-04-01 19:41:13 -0600

    Adding the test file

--- labibTest.java
+++ labibTest.java
@@ -1,3 +1,17 @@
 public class labibTest {
-    //Hello 
+    public class labibTest {
+    @Test 
+    public void weeklyServicesWorks() {
+        if (familyProfile.mobility == true) {
+            int numberofFamilies = 2;
+
+            CreateMultipleHamper multipleHamper = new CreateMultipleHamper();
+            int numberofHampers = multipleHamper.CalculateHamper();
+            assertEquals(2, numberofHampers);
+        }
+        
+        else {
+            assertFalse("Weekly service is not needed", false) 
+        }
+    }
 }
