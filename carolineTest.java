56961a4f904a22e0478ab738a84fcd9f0b87f2e5
CarolineBasta <carolinebasta95@gmail.com>
2022-04-03 18:17:23 -0600

    new changes

--- carolineTest.java
+++ carolineTest.java
@@ -5,20 +5,19 @@ import jdk.jfr.Timestamp;
 import org.junit.Assert;
 import static org.junit.Assert.*;
 
-
 public class carolineTest {
     //testing for shortage in inventory 
     @Test
     public void testShortageException(){
-        Inventroy inventory = new Inventory ("inventory.sql");
+        Inventory inventory = new Inventory ("inventory.sql");
         inventory.getStock(Inventory);
 
         try{
             inventory.ShortageException();
             fail("Error: shortage in inventory");
         }
-        catch (Excetion e){
-            assertTrue(True);
+        catch (Exception e){
+            assertTrue(true);
         }
     }
 }
