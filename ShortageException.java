ec693a27487612dea0a7d3319f1e82f5c0a3c69c
CarolineBasta <carolinebasta95@gmail.com>
2022-04-14 16:22:56 -0600

    shotage exception

--- ShortageException.java
+++ ShortageException.java
@@ -1,6 +1,6 @@
 import java.lang.Exception;
 
-public class ShortageException {
+public class ShortageException extends Exception {
     public ShortageException(){
         super("Inventory is short, can't retrieve items.");
     }
