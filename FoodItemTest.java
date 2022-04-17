e12037f0f3a21b57651c0d7edbd54587244cfe7e
CarolineBasta <carolinebasta95@gmail.com>
2022-04-17 17:27:20 -0600

    added food item testing for FoodItem.java file

--- FoodItemTest.java
+++ FoodItemTest.java
@@ -1,1 +1,79 @@
-//TestFoodItem
+/**
+ @author Jannine Osman <a href="mailto:jannine.osman@ucalgary.ca">jannine.osman@ucalgary.ca</a>
+         Sukriti Sharma <a href="mailto:sukriti.sharma@ucalgary.ca">sukriti.sharma@ucalgary.ca</a>
+         Caroline Basta <a href="mailto:caroline.basta1@ucalgary.ca">caroline.basta1@ucalgary.ca</a>
+         Labib Afsar Ahmed <a href="mailto:labibafsar.ahmed@ucalgary.ca">labibafsar.ahmed@ucalgary.ca</a>
+ @version       1.3
+ @since         1.0
+ */
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.*;
+import java.util.*;
+
+public class FoodItemTest{
+ // getItemID
+ @Test 
+    public void testGetItemID(){
+        FoodItem ourInventory = new FoodItem(51, "Cantaloupe, dozen", 0, 100, 0, 0, 10);
+        int itemIDValue = ourInventory.getId();
+        int expectedItemIDValue = 51;
+        assertEquals("Method getItemID did not return the expected result:", expectedItemIDValue, itemIDValue);
+
+ }
+
+ // test for getName of inventory item
+ @Test
+    public void testGetName(){
+    FoodItem ourInventory = new FoodItem(51, "Cantaloupe, dozen", 0, 100, 0, 0, 10);
+    String itemName = ourInventory.getName();
+    String expectedName = "Cantaloupe, dozen";
+    assertEquals("Method getName did not return the expected result:", expectedName, itemName);
+ }
+
+ // test for getGrain content for inventory item
+ @Test 
+    public void testGetGrain(){
+    FoodItem ourInventory = new FoodItem(51, "Cantaloupe, dozen", 0, 100, 0, 0, 10);
+    int itemGrain = ourInventory.getGrainContent();
+    int expectedItemGrain = 0;
+    assertEquals("Method getGrain did not return the expected result:", expectedItemGrain, itemGrain);
+ }
+
+ // test for getFruitsVeggies content for inventory item
+ @Test 
+    public void testGetFruitsVeggies(){
+    FoodItem ourInventory = new FoodItem(51, "Cantaloupe, dozen", 0, 100, 0, 0, 10);
+    int itemFV = ourInventory.getFVContent();
+    int expectedItemFV = 100;
+    assertEquals("Method getFruitsVeggies did not return the expected result:", expectedItemFV, itemFV);
+ }
+
+ // test for getProtein content for inventory item
+ @Test 
+    public void testGetProtein(){
+    FoodItem ourInventory = new FoodItem(51, "Cantaloupe, dozen", 0, 100, 0, 0, 10);
+    int itemProtein = ourInventory.getProContent();
+    int expectedItemProtein = 0;
+    assertEquals("Method getProtein did not return the expected result:", expectedItemProtein, itemProtein);
+ }
+
+ // test for getOtherContent for inventory item
+ @Test 
+    public void testGetOtherContent(){
+    FoodItem ourInventory = new FoodItem(51, "Cantaloupe, dozen", 0, 100, 0, 0, 10);
+    int itemOtherContent = ourInventory.getOther();
+    int expectedItemOtherContent = 0;
+    assertEquals("Method getOtherContent did not return the expected result:", expectedItemOtherContent, itemOtherContent);
+ }
+
+ // test for getStock for inventory item
+ @Test 
+    public void testGetCalories(){
+    FoodItem ourInventory = new FoodItem(51, "Cantaloupe, dozen", 0, 100, 0, 0, 10);
+    int itemCalories = ourInventory.getCalories();
+    int expectedCalories= 10;
+    assertEquals("Method getCalories did not return the expected result:", expectedCalories, itemCalories);
+ }
+}
