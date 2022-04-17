b6a7ec998d6d6670f76af8c043a5f2ab77e798e8
Jannine Osman <jannineosman@Jannines-MacBook-Pro.local>
2022-04-17 17:12:47 -0600

    Finally GUI

--- GUI.java
+++ GUI.java
@@ -1,69 +1,60 @@
-import javax.swing.*;
-import javax.swing.border.Border;
 
+
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.*;
+import java.util.List;
+import java.util.ArrayList;
 
-// our GUI needs:
+// GUI contains
 // * a place for the name to be entered
 // * the date
 // * an option to select number of families
-
 // * an input box for the families to enter composition
 // * a place to request if one family wants multiple hampers configured
 // * finally a place to press enter on the GUI to submit the request for the order form
 
-// fix multiple hamper configuration
-
 public class GUI{
+    private static List<FamilyProfile> requestedFamilies = new ArrayList<>();
+
     static String employeeName = " ";
     static int families;
     static int adultFemales;
     static int adultMales;
     static int childrenOver8Yrs;
     static int childrenUnder8Yrs;
-    static int multipleHampers;
-    static int weeklyServiceHampers;
-    static JTextField employeeIn = new JTextField("First name, last name");
-    //static JSpinner familyNum = new JSpinner();
+    static boolean weeklyServiceHampers;
+    static JTextField employeeIn = new JTextField("               ");
     static JSpinner AdultFemales = new JSpinner();
     static JSpinner AdultMales = new JSpinner();
     static JSpinner childrenOver8 = new JSpinner();
     static JSpinner childrenUnder8 = new JSpinner();
-    static JSpinner multipleHamperSelect = new JSpinner();
-    static JSpinner weeklyServiceNeeded = new JSpinner();
+    static JCheckBox checkbox = new JCheckBox("Weekly service required?");
     static JTextField numOfFams = new JTextField("  ");
-    public static void main(String args[]){
-        
+
+    public static void main(String args[]) {
 
         // creates base frame for other items to go on top
         EventQueue.invokeLater(() -> {
             JFrame frame = new JFrame("Foodbank hamper system");
-            frame.setSize(1000,300);
+            frame.setSize(1000, 300);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            
 
             JPanel buttonPanel = new JPanel();
             JPanel clientPanel = new JPanel();
             JPanel rightPanel = new JPanel();
             JPanel titlePanel = new JPanel();
-            
 
-            JButton submit = new JButton("Submit family");
+            JButton submit = new JButton("Add family");
             JButton doneSubmit = new JButton("Submit final order");
             JLabel instructions = new JLabel("Please enter the required information to produce a hamper order form.");
             JLabel employeeLabel = new JLabel("Please enter employee name:");
-            //JLabel famillies = new JLabel("Please enter the number of families");
             JLabel family1Females = new JLabel("Please enter the number of adult females in family:");
             JLabel familyMales = new JLabel("Please enter the number of adult males in family:");
             JLabel familyOver8 = new JLabel("Please enter the number of children over 8 in family");
             JLabel familyUnder8 = new JLabel("Please enter the number of children under 8 in family");
-            JLabel multipleHampersLbl = new JLabel("Please enter the number of hampers to configure");
-            JLabel weeklyServiceLbl = new JLabel("If there are mobility concerns, please enter the number of hampers desired for weekly service");
-            JLabel numberOfFams = new JLabel("The number of families submitted: ");
-
-            
+            JLabel numberOfFams = new JLabel("The number of families added: ");
 
             buttonPanel.add(submit);
             titlePanel.add(instructions);
@@ -77,27 +68,31 @@ public class GUI{
             rightPanel.add(childrenOver8);
             rightPanel.add(familyUnder8);
             rightPanel.add(childrenUnder8);
-            rightPanel.add(multipleHampersLbl);
-            rightPanel.add(multipleHamperSelect);
-            rightPanel.add(weeklyServiceLbl);
-            rightPanel.add(weeklyServiceNeeded);
+            rightPanel.add(checkbox);
+            //rightPanel.add(weeklyServiceLbl);
+            //rightPanel.add(weeklyServiceNeeded);
 
             buttonPanel.add(doneSubmit);
             buttonPanel.add(doneSubmit);
             buttonPanel.add(numberOfFams);
             buttonPanel.add(numOfFams);
 
+            // MyListener buttonListener = new MyListener();
+            submit.addActionListener((e) -> {
+                submitAction();
+            });
 
-           // MyListener buttonListener = new MyListener();
-             submit.addActionListener( (e)-> {
-             submitAction();
-        });
+            // MyListener buttonListener = new MyListener();
+            // doneSubmit.addActionListener(buttonListener);
+            doneSubmit.addActionListener((e) -> {
+                submitFinalOrder();
+            });
 
-            MyListener buttonListener = new MyListener();
-            doneSubmit.addActionListener(buttonListener);
-            //buttonsPanel.add(myButton);
 
-            //frame.getContentPane().add(BorderLayout.NORTH, buttonsPanel); //layout manager used here (will place it north most of the frame)
+            // buttonsPanel.add(myButton);
+
+            // frame.getContentPane().add(BorderLayout.NORTH, buttonsPanel); //layout
+            // manager used here (will place it north most of the frame)
             frame.getContentPane().add(BorderLayout.WEST, clientPanel);
             frame.getContentPane().add(BorderLayout.PAGE_END, buttonPanel);
             frame.getContentPane().add(BorderLayout.NORTH, titlePanel);
@@ -105,66 +100,91 @@ public class GUI{
             frame.setVisible(true);
         });
 
-
-
     }
 
-    public static FamilyProfile submitAction() {
-        // we will eventually store these in another class 
+    private static void submitFinalOrder() {
+        if ("".equals(employeeName) || requestedFamilies.size() == 0) {
+            JOptionPane.showMessageDialog(null, "Input data!");
+            return;
+        }
+
+        Order order = new Order(employeeName, requestedFamilies);
+        System.out.println(order);
+        try {
+            HamperCreator hamperCreator = new HamperCreator(order);
+            hamperCreator.buildHamper();
+            JOptionPane.showMessageDialog(null, order);
+            System.out.println(order);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+        // resetting families
+        requestedFamilies = new ArrayList<>();
+        numOfFams.setText("0");
+    }
+
+    private static void submitAction() {
+        // we will eventually store these in another class
         families += 1;
         employeeName = employeeIn.getText();
-        adultFemales = (int)AdultFemales.getValue();
-        adultMales = (int)AdultMales.getValue();
-        childrenOver8Yrs = (int)childrenOver8.getValue();
-        childrenUnder8Yrs = (int)childrenUnder8.getValue();
+        adultFemales = (int) AdultFemales.getValue();
+        adultMales = (int) AdultMales.getValue();
+        childrenOver8Yrs = (int) childrenOver8.getValue();
+        childrenUnder8Yrs = (int) childrenUnder8.getValue();
+        if(checkbox.isSelected()){
+            weeklyServiceHampers = true;
+        }else{
+            weeklyServiceHampers = false;
+        }
         
-        multipleHampers = (int)multipleHamperSelect.getValue();
-        weeklyServiceHampers = (int)weeklyServiceNeeded.getValue();
+        //weeklyServiceHampers = (int) weeklyServiceNeeded.getValue();
 
-        
-
-        
-
- 
-        //FamilyProfile family = new FamilyProfile(adultFemales, adultMales, childrenOver8Yrs, childrenUnder8Yrs, multipleHampers, weeklyServiceHampers);
+        FamilyProfile family = new FamilyProfile(adultFemales, adultMales, childrenOver8Yrs, childrenUnder8Yrs, weeklyServiceHampers);
+        requestedFamilies.add(family);
         // do actions to create hamper for them here too
+
+        // System.out.println(employeeName);
+        // System.out.println(families);
+        // System.out.println(adultFemales);
+        // System.out.println(adultMales);
+        // System.out.println(childrenOver8Yrs);
+        // System.out.println(childrenUnder8Yrs);
+        // System.out.println(multipleHampers);
+        // System.out.println(weeklyServiceHampers);
 
         AdultFemales.setValue(0);
         AdultMales.setValue(0);
         childrenOver8.setValue(0);
         childrenUnder8.setValue(0);
-        multipleHamperSelect.setValue(0);
-        weeklyServiceNeeded.setValue(0);
+        //multipleHamperSelect.setValue(0);
         numOfFams.setText(Integer.toString(families));
-
-       // return new FamilyProfile(adultMales, adultFemales, childrenUnder8Yrs, childrenOver8Yrs );
     }
 }
 
-class MyListener implements ActionListener, MouseListener{
+class MyListener implements ActionListener, MouseListener {
 
-    public void actionPerformed(ActionEvent event){
+    public void actionPerformed(ActionEvent event) {
         JOptionPane.showMessageDialog(null, "The order was submitted");
+    }
+
+    public void mouseClicked(MouseEvent event) {
 
     }
 
-    public void mouseClicked(MouseEvent event){
-        
-    }
-
-    public void mouseEntered(MouseEvent event){
+    public void mouseEntered(MouseEvent event) {
 
     }
 
-    public void mouseExited(MouseEvent event){
+    public void mouseExited(MouseEvent event) {
 
     }
 
-    public void mousePressed(MouseEvent event){
+    public void mousePressed(MouseEvent event) {
 
     }
 
-    public void mouseReleased(MouseEvent event){
+    public void mouseReleased(MouseEvent event) {
 
     }
 
