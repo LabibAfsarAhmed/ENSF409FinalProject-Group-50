402896f1caa5faa46b4b9cc078ee1e3bc901efe4
Sukriti sharma <ss1106connellycourt@gmail.com>
2022-04-17 15:10:39 -0600

    author doc template

---
+++ DailyNeedTest.java
@@ -0,0 +1,23 @@
+
+/**
+ @author Jannine Osman <a href="mailto:jannine.osman@ucalgary.ca">jannine.osman@ucalgary.ca</a>
+         Sukriti Sharma <a href="mailto:sukriti.sharma@ucalgary.ca">sukriti.sharma@ucalgary.ca</a>
+         Caroline Basta <a href="mailto:caroline.basta1@ucalgary.ca">caroline.basta1@ucalgary.ca</a>
+         Labib Afsar Ahmed <a href="mailto:labibafsar.ahmed@ucalgary.ca">labibafsar.ahmed@ucalgary.ca</a>
+ @version       1.3
+ @since         1.0
+ */
+
+//import static org.junit.Assert.*;
+//import org.junit.*;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+public class DailyNeedTest{
+
+
+}
