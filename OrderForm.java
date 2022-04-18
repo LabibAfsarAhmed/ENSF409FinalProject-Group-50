f9c9add66c61c8380ed41a78d90c67f5e5e24302
ss1106 <75515505+ss1106@users.noreply.github.com>
2022-04-17 20:07:43 -0600

    Create OrderForm.java

---
+++ OrderForm.java
@@ -0,0 +1,33 @@
+package edu.ucalgary.ensf409;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class OrderForm {
+
+    private Order order;
+
+    public OrderForm(Order order) {
+        this.order = order;
+        outputToText();
+    }
+
+    public void outputToText() {
+        try {
+            File obj = new File("orderform.txt"); //creates a new File called orderform.txt
+            FileWriter fw = new FileWriter(obj);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(order.toString()); // writes the order string to the orderform file
+            bw.flush();
+            bw.close();
+        } catch (FileNotFoundException ex) { // throws exception if it is unable to open the orderform file
+            System.out.print("unable to open file");
+
+        } catch (IOException ex) { // throws error if it is unable to write to orderform.txt
+            System.out.print("error writing to file");
+        }
+    }
+}
