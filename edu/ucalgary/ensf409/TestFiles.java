2dd30936a2a0bc893eabf0ec363945c20bb13aa2
Sukriti sharma <ss1106connellycourt@gmail.com>
2022-04-18 18:46:44 -0600

    add doc

--- edu/ucalgary/ensf409/TestFiles.java
+++ edu/ucalgary/ensf409/TestFiles.java
@@ -1,3 +1,11 @@
+/**
+ @author Jannine Osman <a href="mailto:jannine.osman@ucalgary.ca">jannine.osman@ucalgary.ca</a>
+         Sukriti Sharma <a href="mailto:sukriti.sharma@ucalgary.ca">sukriti.sharma@ucalgary.ca</a>
+         Caroline Basta <a href="mailto:caroline.basta1@ucalgary.ca">caroline.basta1@ucalgary.ca</a>
+         Labib Afsar Ahmed <a href="mailto:labibafsar.ahmed@ucalgary.ca">labibafsar.ahmed@ucalgary.ca</a>
+ @version       7.8
+ @since         1.0
+ */
 package edu.ucalgary.ensf409;
 
 import static org.junit.Assert.*;
